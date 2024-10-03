9609de02aa08bc2b3106da8f5bca1a04b6aff98e
Maykal Tenev <michaeltenev@gmail.com>
2024-10-03 16:49:16 +0200

    lab from 2 to 6

---
+++ 01_first_steps/03_RectangleArea/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int area = a * b;
+Console.WriteLine(area);
--- 01_first_steps/MyApp/Program.cs
+++ 01_first_steps/MyApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = int.Parse(Console.ReadLine());
-int area = a * a;
-Console.WriteLine(area);
+// int a = int.Parse(Console.ReadLine());
+// int area = a * a;
+// double b = double.Parse(Console.ReadLine());
+// double areaDouble = b * b;
+// Console.WriteLine(areaDouble);
+
+int a = 15;
+int b = 2;
+int result1 = a / b;
+Console.WriteLine(result1);
