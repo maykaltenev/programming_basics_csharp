0825f45a578a4a5f85b09773c3cf61520540e365
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 13:14:52 +0200

    06_Repeainting and 07_FoodDelivery and 08_BasketballEquipment

---
+++ 01_FirstSteps/01_Exercise/07_FoodDelivery/Program.cs
@@ -0,0 +1,18 @@
+﻿double numberChickenMenu = double.Parse(Console.ReadLine());
+double numberFishMenu = double.Parse(Console.ReadLine());
+double numberVegetarianMenu = double.Parse(Console.ReadLine());
+
+double priceChickenMenu = numberChickenMenu * 10.35;
+
+double priceFishMenu = numberFishMenu * 12.40;
+double priceVegetarianMenu = numberVegetarianMenu * 8.15;
+
+double priceMenus = priceChickenMenu + priceFishMenu + priceVegetarianMenu;
+
+double priceDeserts = priceMenus * 0.20;
+
+double priceDelivery = 2.50;
+
+double totalSum = priceMenus + priceDeserts + priceDelivery;
+
+Console.WriteLine(totalSum);
---
+++ 01_FirstSteps/01_Exercise/08_BasketballEquipment/Program.cs
@@ -0,0 +1,9 @@
+﻿double yearlyPriceBasketballTraining = double.Parse(Console.ReadLine());
+
+double shoes = yearlyPriceBasketballTraining - (yearlyPriceBasketballTraining * 0.40);
+double clothes = shoes - (shoes * 0.20);
+double ball = clothes * 0.25;
+double accessories = ball * 0.20;
+double total = yearlyPriceBasketballTraining + shoes + clothes + ball + accessories;
+
+Console.WriteLine(total);
