da48260555d5f5556ef7f520bc17fcb5c8c6ebd5
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 00:38:14 +0200

    04_PasswordGuess

---
+++ 02_Conditional_Statements/04_PasswordGuess/Program.cs
@@ -0,0 +1,11 @@
+﻿string password = "s3cr3t!P@ssw0rd";
+string input = Console.ReadLine();
+
+if (password == input)
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
