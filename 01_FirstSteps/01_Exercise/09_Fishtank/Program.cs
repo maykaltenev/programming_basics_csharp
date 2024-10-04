6e027e2dc1dacf89e96849e3335b0db25d2b445b
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 13:21:19 +0200

    09_Fishtank

---
+++ 01_FirstSteps/01_Exercise/09_Fishtank/Program.cs
@@ -0,0 +1,11 @@
+﻿double length = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+double volume = length * width * height;
+double volumeInLiters = volume * 0.001;
+double takenSpace = percent / 100;
+double litersNeeded = volumeInLiters * (1 - takenSpace);
+
+Console.WriteLine(litersNeeded);
