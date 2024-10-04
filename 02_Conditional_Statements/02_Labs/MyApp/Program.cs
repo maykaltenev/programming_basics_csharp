c0b5025359a262bb02d2ed1f674f13cc4b7df8ce
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 14:39:40 +0200

    01_ExcellentResult

---
+++ 02_Conditional_Statements/02_Labs/MyApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// string number = "string";
+
+// int a = 5;
+// int b = 2;
+// double result = a / b;
+
+// Console.WriteLine(result);
+int a = 5;
+string number = "5";
+Console.WriteLine(a == number);
