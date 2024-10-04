3c54f71ec01b3bcea0f942c4c35f9a879b1a6013
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 11:59:22 +0200

    05_SuppliesForSchool

---
+++ 01_FirstSteps/01_Exercice/05_SuppliesForSchool/Program.cs
@@ -0,0 +1,15 @@
+﻿double pens = double.Parse(Console.ReadLine());
+double markers = double.Parse(Console.ReadLine());
+double cleaningMaterial = double.Parse(Console.ReadLine());
+double discount = double.Parse(Console.ReadLine());
+
+double pricePens = pens * 5.80;
+double priceMarkers = markers * 7.20;
+double priceCleaningMaterial = cleaningMaterial * 1.20;
+double priceAllMaterial = pricePens + priceMarkers + priceCleaningMaterial;
+
+double discountAsRealNumber = discount / 100;
+
+double priceWithDiscount = priceAllMaterial - (priceAllMaterial * discountAsRealNumber);
+
+Console.WriteLine(priceWithDiscount);
