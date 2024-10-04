0376d958e437082b26df27dad99673db28730773
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 00:41:21 +0200

    05_Number100to200

--- 02_Conditional_Statements/05_Number100to200/Program.cs
+++ 02_Conditional_Statements/05_Number100to200/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Less than 100");
+}
+else if (number >= 100 && number <= 200)
+{
+    Console.WriteLine("Between 100 and 200");
+}
+else
+{
+    Console.WriteLine("Greater than 200");
+}
