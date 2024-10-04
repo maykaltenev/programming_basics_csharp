c0d5984462f96b847456bb67e0850fc7a475b1a6
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 11:46:52 +0200

    03_DepositCalculator

---
+++ 01_FirstSteps/01_Exercice/03_DepositCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿double depositSum = double.Parse(Console.ReadLine());
+double depositTerm = double.Parse(Console.ReadLine());
+double annualInterestRate = double.Parse(Console.ReadLine());
+
+double interestRate = depositSum * (annualInterestRate / 100);
+double interestRatePerMonth = interestRate / 12;
+double totalSum = depositSum + depositTerm * interestRatePerMonth;
+
+Console.WriteLine(totalSum);
