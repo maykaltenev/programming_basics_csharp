493c67c7b052e243a40aab427ab756ed32c6dade
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 00:31:55 +0200

    02_GreaterNumber

--- 02_Conditional_Statements/02_Labs/MyApp/Program.cs
+++ 02_Conditional_Statements/02_Labs/MyApp/Program.cs
@@ -5,6 +5,14 @@
 // double result = a / b;
 
 // Console.WriteLine(result);
-int a = 5;
-string number = "5";
-Console.WriteLine(a == number);
+// int a = 5;
+// string number = "5";
+
+
+double number = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Math.Floor-> {Math.Floor(number)}");
+Console.WriteLine($"Math.Ceiling-> {Math.Ceiling(number)}");
+Console.WriteLine($"Math.Truncate-> {Math.Truncate(number)}");
+Console.WriteLine($"Math.Round up to 1 digit-> {Math.Round(number, 1)}");
+Console.WriteLine($"Formatting up to 1 digit-> {number:f5}");
