591c0b3fd14228ca1fecc8c013171794f95da23c
Maykal Tenev <michaeltenev@gmail.com>
2024-10-04 11:52:02 +0200

    04_VacationBookList

---
+++ 01_FirstSteps/01_Exercice/04_VacationBooksList/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberOfPagesCurrentBook = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int totalDays = int.Parse(Console.ReadLine());
+
+int totalTimeReading = numberOfPagesCurrentBook / pagesPerHour;
+int timeNeededInHours = totalTimeReading / totalDays;
+
+Console.WriteLine(timeNeededInHours);
