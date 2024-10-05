e2dbcd36732c0bbc3250c1be2a5ce96f33602e18
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 13:11:28 +0200

    05_GodzillaVsKon

--- 02_Conditional_Statements/02_Exercise/04_ToyShop/Program.cs
+++ 02_Conditional_Statements/02_Exercise/04_ToyShop/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿double puzzle = 2.60;
+double doll = 3;
+double bear = 4.10;
+double minion = 8.20;
+double truck = 2;
+
+double priceTrip = double.Parse(Console.ReadLine());
+double numberPuzzles = double.Parse(Console.ReadLine());
+double numberDolls = double.Parse(Console.ReadLine());
+double numberBears = double.Parse(Console.ReadLine());
+double numberMinions = double.Parse(Console.ReadLine());
+double numberTrucks = double.Parse(Console.ReadLine());
+
+double totalNumberToys = numberPuzzles + numberDolls + numberBears + numberMinions + numberTrucks;
+
+double discount = 0.0;
+
+double costPuzzle = numberPuzzles * puzzle;
+double costDoll = numberDolls * doll;
+double costBear = numberBears * bear;
+double costMinion = numberMinions * minion;
+double costTruck = numberTrucks * truck;
+
+double totalWin = costPuzzle + costDoll + costBear + costMinion + costTruck;
+
+if (totalNumberToys >= 50)
+{
+    discount = totalWin * 0.25;
+    totalWin -= discount;
+}
+double afterRent = totalWin * 0.90;
+double result = Math.Abs(afterRent - priceTrip);
+if (afterRent >= priceTrip)
+{
+    Console.WriteLine($"Yes! {result:f2} lv left.");
+}
+else
+{
+    Console.WriteLine($"Not enough money! {result:f2} lv needed.");
+}
---
+++ 02_Conditional_Statements/02_Exercise/05_GodzillaVsKong/Program.cs
@@ -0,0 +1,25 @@
+﻿double budget = double.Parse(Console.ReadLine());
+double people = double.Parse(Console.ReadLine());
+double priceClothOnePerson = double.Parse(Console.ReadLine());
+
+double decor = budget * 0.10;
+double clothPrice = priceClothOnePerson * people;
+double discount = 0.0;
+if (people > 150)
+{
+    discount = clothPrice * 0.10;
+    clothPrice -= discount;
+}
+
+double totalCost = clothPrice + decor;
+double result = Math.Abs(budget - totalCost);
+if (budget >= totalCost)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {result:f2} leva left.");
+}
+else
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {result:f2} leva more.");
+}
