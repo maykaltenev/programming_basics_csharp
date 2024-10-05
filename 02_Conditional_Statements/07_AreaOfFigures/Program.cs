e2e482871497822c76016fd07aa64332386eed21
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 11:25:40 +0200

    AreaOfFigures

---
+++ 02_Conditional_Statements/07_AreaOfFigures/Program.cs
@@ -0,0 +1,33 @@
+﻿string figureName = Console.ReadLine();
+
+if (figureName == "square")
+{
+    double width = double.Parse(Console.ReadLine());
+    double area = width * width;
+    Console.WriteLine($"{area:f3}");
+}
+else if (figureName == "rectangle")
+{
+    double width = double.Parse(Console.ReadLine());
+    double length = double.Parse(Console.ReadLine());
+    double area = width * length;
+    Console.WriteLine($"{area:f3}");
+}
+else if (figureName == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+
+    double area = radius * radius * Math.PI;
+    Console.WriteLine($"{area:f3}");
+}
+else if (figureName == "triangle")
+{
+    double width = double.Parse(Console.ReadLine());
+    double length = double.Parse(Console.ReadLine());
+    double area = width * length / 2;
+    Console.WriteLine($"{area:f3}");
+}
+else
+{
+    Console.WriteLine($"Unknown Figure");
+}
