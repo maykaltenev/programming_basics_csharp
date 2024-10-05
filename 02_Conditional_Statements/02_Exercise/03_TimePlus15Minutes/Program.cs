6f1105097ad37bdc05e5741454b580f4e7c1eff0
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 12:13:00 +0200

    03_TimePlus15Minutes

---
+++ 02_Conditional_Statements/02_Exercise/03_TimePlus15Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+int totalMinutes = minutes + 15;
+
+if (totalMinutes >= 60)
+{
+    hours++;
+    totalMinutes -= 60;
+}
+
+if (hours > 23)
+{
+    hours = 0;
+}
+if (totalMinutes < 10)
+{
+    Console.Write($"{hours}:0{totalMinutes}");
+}
+else
+{
+    Console.Write($"{hours}:{totalMinutes}");
+}
