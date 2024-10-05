ce66fd78853daa4228b454b4c54310cdd283229d
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 11:34:17 +0200

    01_SumSeconds

---
+++ 02_Conditional_Statements/02_Exercice/01_SumSeconds/Program.cs
@@ -0,0 +1,15 @@
+﻿int inputOne = int.Parse(Console.ReadLine());
+int inputTwo = int.Parse(Console.ReadLine());
+int inputThree = int.Parse(Console.ReadLine());
+
+int total = inputOne + inputTwo + inputThree;
+int minutes = total / 60;
+int seconds = total % 60;
+if (seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
