02e805d49378848a55afe00bef18b7c0efc292fe
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 11:57:57 +0200

    02_BonusScore

---
+++ 02_Conditional_Statements/02_Exercise/02_BonusScore/Program.cs
@@ -0,0 +1,28 @@
+﻿int startPoints = int.Parse(Console.ReadLine());
+
+double bonus = 0.0;
+
+if (startPoints <= 100)
+{
+    bonus = 5;
+}
+else if (startPoints > 1000)
+{
+    bonus = startPoints * 0.10;
+}
+else
+{
+    bonus = startPoints * 0.20;
+}
+
+if (startPoints % 2 == 0)
+{
+    bonus += 1;
+}
+if (startPoints % 10 == 5)
+{
+    bonus += 2;
+}
+
+Console.WriteLine(bonus);
+Console.WriteLine(startPoints + bonus);
---
+++ 02_Conditional_Statements/02_Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿int startPoints = Console.ReadLine();
+int bonus = 0;
+∂
+if (startPoints <= 100)
+{
+    bonus += 5;
+}
+else if (startPoints > 100)
+{
+    bonus += startPoints * 0.20;
+}
+else if (startPoints > 1000)
+{
+    bonus += startPoints * 0.10;
+}
+
+if (startPoints % 2 == 0)
+{
+    bonus += 1;
+}
+if (startPoints % 10 == 5)
+{
+    bonus += 2;
+}
+
+Console.WriteLine(bonus);
+Console.WriteLine($"{startPoints + bonus}");
