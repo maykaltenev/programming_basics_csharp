ed61782e4bda9be3dbc120be52516a571aa3e3a5
Maykal Tenev <michaeltenev@gmail.com>
2024-10-05 13:33:17 +0200

    06_WorldSwimmingRecord

---
+++ 02_Conditional_Statements/02_Exercise/06_WorldSwimmingRecord/Program.cs
@@ -0,0 +1,19 @@
+﻿double recordInSeconds = double.Parse(Console.ReadLine());
+double lengthInMeters = double.Parse(Console.ReadLine());
+double timeOneMeter = double.Parse(Console.ReadLine());
+
+double distanceTime = lengthInMeters * timeOneMeter;
+
+double additionalTime = Math.Floor(lengthInMeters / 15) * 12.5;
+
+double totalTime = distanceTime + additionalTime;
+double result = Math.Abs(totalTime - recordInSeconds);
+
+if (totalTime < recordInSeconds)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+}
+else
+{
+    Console.WriteLine($"No, he failed! He was {result:f2} seconds slower.");
+}
