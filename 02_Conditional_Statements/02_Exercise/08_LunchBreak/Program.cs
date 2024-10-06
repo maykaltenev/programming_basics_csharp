ddde58f9c58b3f263419b7a01de69bab0e8a5e5d
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 11:35:59 +0200

    08_LunchBreak

---
+++ 02_Conditional_Statements/02_Exercise/08_LunchBreak/Program.cs
@@ -0,0 +1,24 @@
+﻿string name = Console.ReadLine();
+double episodeTime = double.Parse(Console.ReadLine());
+double breakTime = double.Parse(Console.ReadLine());
+
+double lunchTime = breakTime * 0.125;
+
+double relaxTime = breakTime * 0.25;
+
+double totalBreakTime = lunchTime + relaxTime;
+
+double resultTime = breakTime - totalBreakTime;
+
+if (resultTime >= episodeTime)
+{
+    Console.WriteLine(
+        $"You have enough time to watch {name} and left with {Math.Ceiling(resultTime - episodeTime)} minutes free time."
+    );
+}
+else
+{
+    Console.WriteLine(
+        $"You don't have enough time to watch {name}, you need {Math.Ceiling(episodeTime - resultTime)} more minutes."
+    );
+}
