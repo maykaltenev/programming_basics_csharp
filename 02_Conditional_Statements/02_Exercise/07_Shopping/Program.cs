0c52659538a21a60f4cd1a17cd39366577558f20
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 11:17:50 +0200

    07_shopping

---
+++ 02_Conditional_Statements/02_Exercise/07_Shopping/Program.cs
@@ -0,0 +1,30 @@
+﻿double budget = double.Parse(Console.ReadLine());
+double videoCards = double.Parse(Console.ReadLine());
+double processors = double.Parse(Console.ReadLine());
+double memory = double.Parse(Console.ReadLine());
+
+double costVideoCard = 250;
+double totalVideoCard = videoCards * 250;
+
+double priceProcessors = totalVideoCard * 0.35;
+double priceMemory = totalVideoCard * 0.10;
+
+double totalProcessors = processors * priceProcessors;
+double totalMemory = memory * priceMemory;
+
+double totalSum = totalMemory + totalVideoCard + totalProcessors;
+
+if (videoCards > processors)
+{
+    totalSum *= 0.85;
+}
+
+double result = Math.Abs(budget - totalSum);
+if (budget >= totalSum)
+{
+    Console.WriteLine($"You have {result:f2} leva left!");
+}
+else
+{
+    Console.WriteLine($"Not enough money! You need {result:f2} leva more!");
+}
