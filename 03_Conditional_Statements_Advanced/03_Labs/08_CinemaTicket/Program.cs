6562caf5c8c4e7f1d5a6bd36f09ef839e9ca5cb4
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 12:28:38 +0200

    08_CinemaTicket

---
+++ 03_Conditional_Statements_Advanced/03_Labs/08_CinemaTicket/Program.cs
@@ -0,0 +1,14 @@
+﻿string dayOfWeek = Console.ReadLine();
+
+if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Friday")
+{
+    Console.WriteLine(12);
+}
+else if (dayOfWeek == "Wednesday" || dayOfWeek == "Thursday")
+{
+    Console.WriteLine(14);
+}
+else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+{
+    Console.WriteLine(16);
+}
