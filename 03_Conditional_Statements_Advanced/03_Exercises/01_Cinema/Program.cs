6c0c4f33173ca199e5c62eb0b9ad8549d4e8d515
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 14:03:05 +0200

    01_Cinema

---
+++ 03_Conditional_Statements_Advanced/03_Exercises/01_Cinema/Program.cs
@@ -0,0 +1,21 @@
+﻿string typeCinema = Console.ReadLine();
+int r = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+double price = 0.0;
+if (typeCinema == "Premiere")
+{
+    price = 12.00;
+}
+else if (typeCinema == "Normal")
+{
+    price = 7.50;
+}
+else if (typeCinema == "Discount")
+{
+    price = 5.00;
+}
+
+double total = r * c * price;
+
+Console.WriteLine($"{total:f2} leva");
