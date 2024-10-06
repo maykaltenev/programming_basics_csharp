7ba748902128d7dad489189721a52322eb247706
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 12:23:26 +0200

    07_WorkingHours

---
+++ 03_Conditional_Statements_Advanced/03_Labs/07_WorkingHours/Program.cs
@@ -0,0 +1,25 @@
+﻿int timeOfDay = int.Parse(Console.ReadLine());
+string dayOfWeek = Console.ReadLine();
+
+if (
+    dayOfWeek == "Monday"
+    || dayOfWeek == "Tuesday"
+    || dayOfWeek == "Wednesday"
+    || dayOfWeek == "Thursday"
+    || dayOfWeek == "Friday"
+    || dayOfWeek == "Saturday"
+)
+{
+    if (timeOfDay >= 10 && timeOfDay <= 18)
+    {
+        Console.WriteLine("open");
+    }
+    else
+    {
+        Console.WriteLine("closed");
+    }
+}
+else
+{
+    Console.WriteLine("closed");
+}
