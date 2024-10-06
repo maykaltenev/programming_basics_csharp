f6cb3bd035a3e595a18fc7dadb4e5ad4d20ed286
Maykal Tenev <michaeltenev@gmail.com>
2024-10-06 12:32:59 +0200

    09_FruitOrVegetable

---
+++ 03_Conditional_Statements_Advanced/03_Labs/09_FruitOrVegetable/Program.cs
@@ -0,0 +1,21 @@
+﻿string item = Console.ReadLine();
+
+if (
+    item == "banana"
+    || item == "apple"
+    || item == "kiwi"
+    || item == "cherry"
+    || item == "lemon"
+    || item == "grapes"
+)
+{
+    Console.WriteLine("fruit");
+}
+else if (item == "tomato" || item == "cucumber" || item == "pepper" || item == "carrot")
+{
+    Console.WriteLine("vegetable");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
