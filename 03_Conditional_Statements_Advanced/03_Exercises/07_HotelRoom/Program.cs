10ec1427ebe0bfac692bd7bc2837c6dcb462ffb7
Maykal Tenev <michaeltenev@gmail.com>
2024-10-08 00:31:28 +0200

    08_OnTimeForExam

--- 03_Conditional_Statements_Advanced/03_Exercises/07_HotelRoom/Program.cs
+++ 03_Conditional_Statements_Advanced/03_Exercises/07_HotelRoom/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string month = Console.ReadLine();
+int numberOfDays = int.Parse(Console.ReadLine());
+
+double priceStudio = 1;
+double priceApartment = 1;
+double studioDiscount = 1;
+double apartmentDiscount = 1;
+if (month == "May" || month == "October")
+{
+    priceStudio = 50;
+    priceApartment = 65;
+
+    if (numberOfDays > 7 && numberOfDays < 14)
+    {
+        studioDiscount = 0.95;
+    }
+    else if (numberOfDays > 14)
+    {
+        studioDiscount = 0.70;
+        apartmentDiscount = 0.90;
+    }
+}
+else if (month == "June" || month == "September")
+{
+    priceStudio = 75.20;
+    priceApartment = 68.70;
+    if (numberOfDays > 14)
+    {
+        studioDiscount = 0.80;
+        apartmentDiscount = 0.90;
+    }
+}
+else if (month == "July" || month == "August")
+{
+    priceStudio = 76;
+    priceApartment = 77;
+
+    if (numberOfDays > 14)
+    {
+        apartmentDiscount = 0.90;
+    }
+}
+
+double resultStudio = priceStudio * numberOfDays * studioDiscount;
+double resultApartment = priceApartment * numberOfDays * apartmentDiscount;
+Console.WriteLine($"Apartment: {resultApartment:f2} lv.");
+Console.WriteLine($"Studio: {resultStudio:f2} lv.");
---
+++ 03_Conditional_Statements_Advanced/03_Exercises/08_OnTimeForExam/Program.cs
@@ -0,0 +1,107 @@
+﻿double hourOfExam = double.Parse(Console.ReadLine());
+double minutesOfExam = double.Parse(Console.ReadLine());
+double hourOfArrival = double.Parse(Console.ReadLine());
+double minutesOfArrival = double.Parse(Console.ReadLine());
+double result = 0.0;
+
+// // Convert hours to minutes
+double totalTimeOfExam = hourOfExam * 60 + minutesOfExam;
+double totalTimeOfArrival = hourOfArrival * 60 + minutesOfArrival;
+
+if (totalTimeOfArrival >= totalTimeOfExam)
+{
+    result = totalTimeOfArrival - totalTimeOfExam;
+    Console.WriteLine(result);
+    double minutesResult = (int)result % 60;
+    double hoursResult = (int)result / 60;
+    hoursResult = hoursResult < 1 ? 0 : hoursResult;
+    if (result >= 1)
+    {
+        Console.WriteLine("Late");
+        if (hoursResult > 0)
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{hoursResult}:0{minutesResult} hours after the start");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursResult}:{minutesResult} hours after the start");
+            }
+        }
+        else
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{minutesResult} minutes after the start");
+            }
+            else
+            {
+                Console.WriteLine($"{minutesResult} minutes after the start");
+            }
+        }
+    }
+    else if (result == 0)
+    {
+        Console.WriteLine("On Time");
+    }
+}
+else if (totalTimeOfExam >= totalTimeOfArrival)
+{
+    result = totalTimeOfExam - totalTimeOfArrival;
+    double minutesResult = (int)result % 60;
+    double hoursResult = (int)result / 60;
+    hoursResult = hoursResult < 1 ? 0 : hoursResult;
+    if (result == 0 || result <= 30)
+    {
+        Console.WriteLine("On time");
+        if (hoursResult > 0)
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{hoursResult}:0{minutesResult} before the start");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursResult}:{minutesResult} before the start");
+            }
+        }
+        else
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{minutesResult} minutes before the start");
+            }
+            else
+            {
+                Console.WriteLine($"{minutesResult} minutes before the start");
+            }
+        }
+    }
+    else if (result > 30)
+    {
+        Console.WriteLine("Early");
+        if (hoursResult > 0)
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{hoursResult}:0{minutesResult} hours before the start");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursResult}:{minutesResult} hours before the start");
+            }
+        }
+        else
+        {
+            if (minutesResult < 10)
+            {
+                Console.WriteLine($"{minutesResult} minutes before the start");
+            }
+            else
+            {
+                Console.WriteLine($"{minutesResult} minutes before the start");
+            }
+        }
+    }
+}
