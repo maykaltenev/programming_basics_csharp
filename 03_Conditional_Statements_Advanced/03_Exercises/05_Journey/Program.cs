e640e9af96f544bcef7de9de549c877b9a12b960
Maykal Tenev <michaeltenev@gmail.com>
2024-10-07 15:33:20 +0200

    05_Journey and 06_OperationsBetweenNumbers

--- 03_Conditional_Statements_Advanced/03_Exercises/05_Journey/Program.cs
+++ 03_Conditional_Statements_Advanced/03_Exercises/05_Journey/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+string destination = "";
+string location = "";
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    if (season == "summer")
+    {
+        location = "Camp";
+        budget *= 0.30;
+    }
+    else if (season == "winter")
+    {
+        location = "Hotel";
+        budget *= 0.70;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    if (season == "summer")
+    {
+        location = "Camp";
+        budget *= 0.40;
+    }
+    else if (season == "winter")
+    {
+        location = "Hotel";
+        budget *= 0.80;
+    }
+}
+else if (budget > 1000)
+{
+    destination = "Europe";
+    location = "Hotel";
+    budget *= 0.90;
+}
+else { }
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{location} - {budget:f2}");
--- 03_Conditional_Statements_Advanced/03_Exercises/06_OperationsBetweenNumbers/Program.cs
+++ 03_Conditional_Statements_Advanced/03_Exercises/06_OperationsBetweenNumbers/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberOne = double.Parse(Console.ReadLine());
+double numberTwo = double.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+double result = 0.0;
+
+string oddOrEven = "";
+
+if (operation == "+")
+{
+    result = numberOne + numberTwo;
+
+    if (result % 2 == 0)
+    {
+        oddOrEven = "even";
+    }
+    else
+    {
+        oddOrEven = "odd";
+    }
+    Console.WriteLine($"{numberOne} {operation} {numberTwo} = {result} - {oddOrEven}");
+}
+else if (operation == "/")
+{
+    if (numberOne == 0 || numberTwo == 0)
+    {
+        double notZero = numberOne == 0 ? numberTwo : numberOne;
+        Console.WriteLine($"Cannot divide {notZero} by zero");
+    }
+    else
+    {
+        result = numberOne / numberTwo;
+        Console.WriteLine($"{numberOne} {operation} {numberTwo} = {result:f2}");
+    }
+}
+else if (operation == "-")
+{
+    result = numberOne - numberTwo;
+
+    if (result % 2 == 0)
+    {
+        oddOrEven = "even";
+    }
+    else
+    {
+        oddOrEven = "odd";
+    }
+    Console.WriteLine($"{numberOne} {operation} {numberTwo} = {result} - {oddOrEven}");
+}
+else if (operation == "%")
+{
+    if (numberOne == 0 || numberTwo == 0)
+    {
+        double notZero = numberOne == 0 ? numberTwo : numberOne;
+        Console.WriteLine($"Cannot divide {notZero} by zero");
+    }
+    else
+    {
+        result = numberOne % numberTwo;
+        Console.WriteLine($"{numberOne} {operation} {numberTwo} = {result}");
+    }
+}
+else if (operation == "*")
+{
+    result = numberOne * numberTwo;
+
+    if (result % 2 == 0)
+    {
+        oddOrEven = "even";
+    }
+    else
+    {
+        oddOrEven = "odd";
+    }
+    Console.WriteLine($"{numberOne} {operation} {numberTwo} = {result} - {oddOrEven}");
+}
