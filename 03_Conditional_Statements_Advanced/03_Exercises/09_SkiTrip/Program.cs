ea73f3274576767a4f271dbe1b8bf3bd85a11f6d
Maykal Tenev <michaeltenev@gmail.com>
2024-10-08 10:23:41 +0200

    09_SkiTrip

--- 03_Conditional_Statements_Advanced/03_Exercises/09_SkiTrip/Program.cs
+++ 03_Conditional_Statements_Advanced/03_Exercises/09_SkiTrip/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double firstDays = double.Parse(Console.ReadLine());
+string accommodationSpace = Console.ReadLine();
+string review = Console.ReadLine();
+double discount = 1.0;
+double result = 0.0;
+double days = firstDays - 1;
+if (accommodationSpace == "apartment")
+{
+    if (days < 10)
+    {
+        discount = 0.70;
+    }
+    else if (days >= 10 && days < 15)
+    {
+        discount = 0.65;
+    }
+    else if (days > 15)
+    {
+        discount = 0.50;
+    }
+
+    result = days * 25 * discount;
+}
+else if (accommodationSpace == "president apartment")
+{
+    if (days < 10)
+    {
+        discount = 0.90;
+    }
+    else if (days >= 10 && days < 15)
+    {
+        discount = 0.85;
+    }
+    else if (days > 15)
+    {
+        discount = 0.80;
+    }
+    result = days * 35 * discount;
+}
+else if (accommodationSpace == "room for one person")
+{
+    result = days * 18;
+}
+
+if (review == "positive")
+{
+    result = result * 1.25;
+}
+else if (review == "negative")
+{
+    result = result * 0.90;
+}
+Console.WriteLine($"{result:f2}");
