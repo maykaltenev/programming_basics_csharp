910e4b4136480f65a2d11a2c605d03e3293a2693
Maykal Tenev <michaeltenev@gmail.com>
2024-10-12 21:05:54 +0200

    06_Exercise

---
+++ 06_NestedLopps/06_Exercise/04_TrainTheTrainers/Program.cs
@@ -0,0 +1,25 @@
+﻿int juries = int.Parse(Console.ReadLine());
+
+double presentationScoresSum = 0;
+int presentationCount = 0;
+
+string presentationName = Console.ReadLine();
+while (presentationName != "Finish")
+{
+    double currentScoresSum = 0;
+    for (int i = 0; i < juries; i++)
+    {
+        double juryScore = double.Parse(Console.ReadLine());
+        currentScoresSum += juryScore;
+    }
+    double grade = currentScoresSum / juries;
+    Console.WriteLine($"{presentationName} - {grade:f2}.");
+
+    presentationScoresSum += grade;
+    presentationCount++;
+
+    presentationName = Console.ReadLine();
+}
+
+double averageAssessment = presentationScoresSum / presentationCount;
+Console.WriteLine($"Student's final assessment is {averageAssessment:f2}.");
